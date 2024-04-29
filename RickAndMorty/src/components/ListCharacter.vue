9f82ade429d48842ca9324a4dfe12eb890a78cb3
Erik Delanda Zaros <127625388+Erik-Zaros@users.noreply.github.com>
2024-04-29 20:07:30 -0300

    Adicionando arquivos do projeto

---
+++ RickAndMorty/src/components/ListCharacter.vue
@@ -0,0 +1,21 @@
+<script setup>
+const character = defineProps(["name", "url", "status", "species", "gender", "location", "episode"])
+const characterId = character.url.split('/')[5]
+const imageUrl= "https://rickandmortyapi.com/api/character/avatar/" + characterId + ".jpeg"
+console.log(imageUrl)
+
+</script>
+
+<template>  
+    <div class="col-md-3">
+        <div class="card p-2 mb-1">
+            <img :src="imageUrl" height="300" alt="">
+            <p class="text-center"><b>Nome:</b> {{character.name}}</p>
+            <p class="text-center"><b>Status:</b> {{character.status}}</p>
+            <p class="text-center"><b>Espécie:</b> {{character.species}}</p>
+            <p class="text-center"><b>Gênero:</b> {{character.gender}}</p>
+            <p class="text-center"><b>Localização Atual:</b> {{character.location.name}}</p>
+            <p class="text-center"><b>Quantidade de Episódios que Apareceu:</b> {{character.episode.length}}</p>
+        </div>
+    </div>
+</template>
---
+++ RickAndMorty/src/views/AboutView.vue
@@ -0,0 +1,44 @@
+<template>
+ <div class="container">
+  <div class="card">
+    <img src="https://s2-techtudo.glbimg.com/QDIYAODrGD6Wx6lO5ALZoplvQJw=/0x0:1200x800/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/w/q/GQtAzUSC23yGNHAaG36A/8641cd99-093f-4f85-9a26-388494eb3a7f.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">About Me</h5>
+      <p class="card-text">Meu nome é Erik Delanda Zaros, tenho 19 anos. Estou cursando Análise e Desenvolvimento de Sistema na Universidade de Márilia (Unimar). Meu Registro de Aluno é 1959937 e estou no 3° semestre da turma C.</p>
+    </div>
+  </div>
+  
+  <div class="card">
+    <img src="https://s2-techtudo.glbimg.com/WmHf3jIWbjOun5-q-YDkjkmuXtc=/0x0:1300x700/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/9/G/t7700wRdWvP6eEzMACAw/rick-e-morty-1.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Hard Skils</h5>
+      <p class="card-text">Domínio intermediário em HTML, CSS, JS, PHP, SQL e Python. Experiência em ferramentas como Bootstrap PostrgreSQL e MySql.</p>
+    </div>
+  </div>
+  
+  <div class="card">
+    <img src="https://tm.ibxk.com.br//ms/images/highlights/000/043/919/41576.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Soft Skils</h5>
+      <p class="card-text">Tenho uma boa comunicação, facilidade de aprender, proativo e trabalho bem em equipe.</p>
+    </div>
+  </div>
+</div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+  .container {
+    max-width: 800px; 
+    margin: 0 auto; 
+    border: 1px solid #ccc;
+    padding: 20px; 
+  } 
+  
+}
+</style>
